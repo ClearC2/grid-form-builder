9a59c3bbb8c94942c7e62a75f5aa2dd11739fad7
Jacob Allen <JacobRAllen1@gmail.com>
2018-02-14 14:31:10 -0600

    refactored textarea to take the inline prop

--- src/FieldDefinitions/Textarea.js
+++ src/FieldDefinitions/Textarea.js
@@ -6,15 +6,59 @@ export default class Textarea extends Component {
     if (this.props.draggable) e.stopPropagation()
   }
   render = () => {
-    const {field, formValues = Map(), handleOnChange = () => {}, opts = {}} = this.props
+    const {inline, field, formValues = Map(), handleOnChange = () => {}, opts = {}} = this.props
     const {rows = 4, label = field, style = {}, labelStyle = {}, Icon = null, iconProps = {}, props = {}} = opts
+
+    const styles = {
+      container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: inline ? 'row' : 'column',
+        background: 'transparent'
+      },
+      labelContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: inline ? 150 : '100%',
+        minWidth: inline ? 150 : '100%',
+        height: 15,
+        marginTop: inline ? 4 : 0,
+        background: 'transparent',
+        ...labelStyle
+      },
+      label: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        lineHeight: inline ? '23px' : '15px',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        fontSize: inline ? '10pt' : '8pt',
+        background: 'transparent',
+        ...labelStyle
+      },
+      icon: {
+        marginRight: 5,
+        width: 20
+      },
+      input: {
+        display: 'flex',
+        flexGrow: 1,
+        paddingLeft: 5,
+        resize: 'none',
+        backgroundColor: 'transparent',
+        minWidth: 90,
+        marginTop: inline ? 25 : 0,
+        ...style
+      }
+    }
+
     return (
-      <div style={{display: 'flex', flex: 1, flexDirection: 'row'}}>
-        <div style={{display: 'flex', flexDirection: 'row', width: 150, minWidth: 150, height: 15, marginTop: 4, ...labelStyle}}>
-          {!!Icon && <Icon size={20} style={{marginRight: 5, width: 20}} {...iconProps} />}
-          <strong style={{display: 'flex', justifyContent: 'flex-start', lineHeight: '23px', whiteSpace: 'nowrap', textOverflow: 'ellipsis', ...labelStyle}}>{label}</strong>
+      <div style={styles.container}>
+        <div style={styles.labelContainer}>
+          {!!Icon && <Icon size={20} style={styles.icon} {...iconProps} />}
+          <strong style={styles.label}>{label}</strong>
         </div>
-        <textarea onMouseDown={this.onMouseDown} onChange={handleOnChange} style={{display: 'flex', flexGrow: 1, paddingLeft: 5, resize: 'none', backgroundColor: 'transparent', minWidth: 90, marginTop: 25, ...style}} name={field} value={formValues.get(field, '')} rows={rows} {...props} />
+        <textarea onMouseDown={this.onMouseDown} onChange={handleOnChange} style={styles.input} name={field} value={formValues.get(field, '')} rows={rows} {...props} />
       </div>
     )
   }
