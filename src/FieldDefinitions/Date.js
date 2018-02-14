37f1270ecb4e3fc0e0448eafc2ba8fac4f21f7a1
Jacob Allen <JacobRAllen1@gmail.com>
2018-02-14 14:49:53 -0600

    refactored date to take the inline prop

--- src/FieldDefinitions/Date.js
+++ src/FieldDefinitions/Date.js
@@ -13,16 +13,52 @@ export default class Date extends Component {
     if (this.props.draggable) e.stopPropagation()
   }
   render = () => {
-    const {field, formValues = Map(), opts = {}} = this.props
+    const {inline, field, formValues = Map(), opts = {}} = this.props
     const {label = field, labelStyle = {}, Icon = null, iconProps = {}, props = {}} = opts
     let value = formValues.get(field, '')
+
+    const styles = {
+      container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: inline ? 'row' : 'column',
+        background: 'transparent'
+      },
+      labelContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: inline ? 150 : '100%',
+        minWidth: inline ? 150 : '100%',
+        height: 15,
+        marginTop: inline ? 4 : 0,
+        background: 'transparent',
+        ...labelStyle
+      },
+      label: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        lineHeight: inline ? '23px' : '15px',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        fontSize: inline ? '10pt' : '8pt',
+        background: 'transparent',
+        ...labelStyle
+      },
+      icon: {
+        marginRight: 5,
+        width: 20
+      }
+    }
+
+    const className = inline ? `date-wrapper-grid-input date-wrapper-grid-input-inline` : `date-wrapper-grid-input`
+
     return (
-      <div style={{display: 'flex', flex: 1, flexDirection: 'row'}}>
-        <div style={{display: 'flex', flexDirection: 'row', width: 150, minWidth: 150, height: 15, marginTop: 4, ...labelStyle}}>
-          {!!Icon && <Icon size={20} style={{marginRight: 5}} {...iconProps} />}
-          <strong style={{display: 'flex', justifyContent: 'flex-start', lineHeight: '23px', whiteSpace: 'nowrap', textOverflow: 'ellipsis', ...labelStyle}}>{label}</strong>
+      <div style={styles.container}>
+        <div style={styles.labelContainer}>
+          {!!Icon && <Icon size={20} style={styles.icon} {...iconProps} />}
+          <strong style={styles.label}>{label}</strong>
         </div>
-        <DateTime onMouseDown={this.onMouseDown} value={value} onChange={this.handleChange} dateFormat='M/D/YYYY' timeFormat={false} className='date-wrapper-grid-input' {...props} />
+        <DateTime onMouseDown={this.onMouseDown} value={value} onChange={this.handleChange} dateFormat='M/D/YYYY' timeFormat={false} className={className} {...props} />
       </div>
     )
   }
