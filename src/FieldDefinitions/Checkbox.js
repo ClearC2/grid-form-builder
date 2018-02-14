1693aac7c3a0cfad79c6b0066556ca7760ca89dc
Jacob Allen <JacobRAllen1@gmail.com>
2018-02-14 16:36:08 -0600

    checkbox now takes inline prop

--- src/FieldDefinitions/Checkbox.js
+++ src/FieldDefinitions/Checkbox.js
@@ -10,13 +10,42 @@ export default class Checkbox extends Component {
   }
 
   render = () => {
-    const {field, formValues = Map(), opts = {}} = this.props
-    const {label = field, style = {}, props = {}} = opts
+    const {inline, field, formValues = Map(), opts = {}} = this.props
+    const {label = field, style = {}, props = {}, labelStyle} = opts
     const value = formValues.get(field, '')
+
+    const styles = {
+      container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'row',
+        background: 'transparent'
+      },
+      label: {
+        display: 'flex',
+        flex: 1,
+        height: inline ? 27 : 40,
+        margin: 0,
+        marginBottom: inline ? 5 : 0,
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: inline ? '10pt' : '8pt',
+        lineHeight: inline ? '10pt' : '8pt',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis'
+      },
+      input: {
+        display: 'flex',
+        marginRight: 5,
+        marginTop: 0,
+        ...style
+      }
+    }
+
     return (
-      <div style={{display: 'flex', flex: 1, flexDirection: 'row'}}>
-        <label key={field} style={{display: 'flex', flex: 1, height: 27, margin: 0, marginBottom: 5, alignItems: 'center', fontWeight: 'bold', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>
-          <input className='checkbox-grid-input' onChange={this.handleOnChange} style={{display: 'flex', marginRight: 5, ...style}} type='checkbox' name={field} checked={value.toLowerCase() === 'on'} {...props} />
+      <div style={styles.container}>
+        <label key={field} style={styles.label}>
+          <input className='checkbox-grid-input' onChange={this.handleOnChange} style={styles.input} type='checkbox' name={field} checked={value.toLowerCase() === 'on'} {...props} />
           {label}
         </label>
       </div>
