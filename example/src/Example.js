402e7a0a47109e4d43fba94382e375f3d363613a
Will Darden <wldarden@gmail.com>
2018-06-27 11:18:39 -0500

    clean up conditionalInput code for readability

--- example/src/Example.js
+++ example/src/Example.js
@@ -471,6 +471,7 @@ export default class Example extends Component {
           formValues={this.state.formValues}
           handleOnChange={this.handleOnChange}
           inline={this.state.inline}
+          conditionalSearch={false}
         />
       </div>
     )
