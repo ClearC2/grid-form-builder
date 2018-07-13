63aaec02abaea01126bae457790503042cf0df87
Jacob Allen <jacobrallen1@gmail.com>
2018-07-13 13:41:28 -0500

    phone can now take a placeholder

--- src/FieldDefinitions/Phone.js
+++ src/FieldDefinitions/Phone.js
@@ -54,10 +54,10 @@ export class Phone extends Component {
     const {label = name} = config
     const value = formValues.get(name, '')
     const warn = requiredWarning && formValues.get(name, '').length === 0 && required
-    let {readonly = false, disabled = false} = config
+    let {readonly = false, disabled = false, placeholder = ''} = config
     disabled = disabled || readonly
 
-    const placeholder = warn ? '* This Field Is Required' : ''
+    placeholder = warn ? '* This Field Is Required' : placeholder
     const className = warn ? 'warn-required' : ''
 
     const styles = {
