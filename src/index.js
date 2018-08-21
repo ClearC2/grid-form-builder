59567686920ae5ece65711da11ddd2f459585f47
David Adams <adams.david.10@gmail.com>
2018-08-21 10:11:19 -0500

    build(imports): remove portal node set required by c2-dialog

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import {setPortalNode} from 'c2-dialog'
 export reducer from './redux'
 export FormDroppable from './FormDroppable'
 export {default as FormBuilder, updateFormValues, initCustomFormComponents, initComponentIconLibrary} from './GridFormBuilder'
 export {initFormBuilderAjax} from './config'
 export {CONDITIONS, TEXT_INPUTS} from './FieldDefinitions/Conditionalinput'
-
-setPortalNode(document.getElementById('dialogs'))
