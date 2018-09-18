b03e191938f4023602e3fa13ae6cbdd86c843611
David Adams <adams.david.10@gmail.com>
2018-09-18 09:36:27 -0500

    build(babel): stop using stage-0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {setPortalNode} from 'c2-dialog'
-export reducer from './redux'
-export FormDroppable from './FormDroppable'
+export {default as reducer} from './redux'
+export {default as FormDroppable} from './FormDroppable'
 export {default as FormBuilder, updateFormValues, initCustomFormComponents, initComponentIconLibrary} from './GridFormBuilder'
 export {initFormBuilderAjax} from './config'
 export {CONDITIONS, TEXT_INPUTS} from './FieldDefinitions/Conditionalinput'
