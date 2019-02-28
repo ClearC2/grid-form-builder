753bf7ee21dfd563487706c379d10de8fcbe2485
Mike Ngu <mikengu01@gmail.com>
2019-02-28 08:12:19 -0600

    feat(validation): cannot save when email is invalid

--- src/FieldDefinitions/Email.js
+++ src/FieldDefinitions/Email.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {Map} from 'immutable'
+import {emailValidator} from '../utils'
 import {DropTarget} from 'react-dnd'
 
 class Email extends Component {
@@ -60,63 +61,8 @@ class Email extends Component {
     handleCascadeKeywordClick(config)
   }
 
-  // This could potentially be extracted into a helper function for reuse across projects.
-  emailValidation = (email) => {
-    // If email is null, undefined, empty array or string
-    if (!email) {
-      return false
-    }
-    email = String(email).trim()
-
-    // https://www.mailboxvalidator.com/resources/articles/acceptable-email-address-syntax-rfc/
-    const emailInput = email.split('@')
-    const emailLocal = emailInput[0].trim()
-
-    // email does not contain "@"
-    if (emailInput.length === 1) {
-      return false
-    }
-
-    // validating local part of the email
-    if (
-      (emailLocal.length > 64) ||
-      (emailLocal.length < 1) ||
-      (emailLocal.includes('..')) ||
-      (emailLocal[0] === '.') ||
-      (emailLocal[emailLocal.length - 1] === '.')
-    ) {
-      return false
-    }
-
-    const domainPart = emailInput[1].trim()
-    const emailDomains = domainPart.split('.')
-
-    // Entire domain part of the email does not contain "." or is greater than 255 characters
-    if (emailDomains.length < 2 || domainPart.length > 255) {
-      return false
-    }
-
-    const validateDomainSegment = (segment) => {
-      // A true here means the domain segment failed validation
-      return ((segment.length > 63) || (segment.length < 1) || (segment[0] === '-') || (segment[segment.length - 1] === '-'))
-    }
-
-    // emailDomains MAY consist of subdomain1.subDomain2.domain.topLevelDomain need to validate each domain segment
-    if (emailDomains.some(validateDomainSegment)) {
-      return false
-    }
-
-    // Top level domains may not be entirely numeric
-    const tld = emailDomains[emailDomains.length - 1]
-    if ((tld.length < 2) || (tld.match(/^[0-9]+$/))) {
-      return false
-    }
-
-    return true
-  }
-
   generateValidationError = value => {
-    if (value) return (!this.emailValidation(value) && this.state.displayError) && '* Invalid email' 
+    if (value) return (!emailValidator(value) && this.state.displayError) && '* Invalid email' 
   }
 
   handleOnChange = () => {}
@@ -196,7 +142,7 @@ class Email extends Component {
         fontSize: '9pt',
         marginLeft: 3,
         marginTop: -1,
-        color: !this.emailValidation(value) && '#ec1c24',
+        color: !emailValidator(value) && '#ec1c24',
         marginRight: 5
       }
     }
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,57 @@ export const timeStamp = () => {
   ms = String(ms).slice(-7)
   return +ms
 }
+
+export const emailValidator = email => {
+  // If email is null, undefined, empty array or string
+  if (!email) {
+    return false
+  }
+  email = String(email).trim()
+
+  // https://www.mailboxvalidator.com/resources/articles/acceptable-email-address-syntax-rfc/
+  const emailInput = email.split('@')
+  const emailLocal = emailInput[0].trim()
+
+  // email does not contain "@"
+  if (emailInput.length === 1) {
+    return false
+  }
+
+  // validating local part of the email
+  if (
+    (emailLocal.length > 64) ||
+    (emailLocal.length < 1) ||
+    (emailLocal.includes('..')) ||
+    (emailLocal[0] === '.') ||
+    (emailLocal[emailLocal.length - 1] === '.')
+  ) {
+    return false
+  }
+
+  const domainPart = emailInput[1].trim()
+  const emailDomains = domainPart.split('.')
+
+  // Entire domain part of the email does not contain "." or is greater than 255 characters
+  if (emailDomains.length < 2 || domainPart.length > 255) {
+    return false
+  }
+
+  const validateDomainSegment = (segment) => {
+    // A true here means the domain segment failed validation
+    return ((segment.length > 63) || (segment.length < 1) || (segment[0] === '-') || (segment[segment.length - 1] === '-'))
+  }
+
+  // emailDomains MAY consist of subdomain1.subDomain2.domain.topLevelDomain need to validate each domain segment
+  if (emailDomains.some(validateDomainSegment)) {
+    return false
+  }
+
+  // Top level domains may not be entirely numeric
+  const tld = emailDomains[emailDomains.length - 1]
+  if ((tld.length < 2) || (tld.match(/^[0-9]+$/))) {
+    return false
+  }
+
+  return true
+}
