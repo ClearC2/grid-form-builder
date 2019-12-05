32d3f638466775b3b1bf6bd60a64ff193ae82f88
Jacob Allen <jacobrallen1@gmail.com>
2019-12-05 17:08:18 -0600

    feat(dnd): adds drag and drop support to the inner cell component to be shared among all inputs

--- src/Inputs/core/InnerCell.js
+++ src/Inputs/core/InnerCell.js
@@ -1,12 +1,45 @@
-import React, {useContext, useCallback} from 'react'
+import React, {useContext, useCallback, useEffect, useRef} from 'react'
 import PropTypes from 'prop-types'
 import {FormValueContext} from '../../FormBuilder'
 import InputContainer from './InputContainer'
 import LabelContainer from './LabelContainer'
 import {mapInputType} from '../index'
+import {DropTarget} from 'react-dnd'
 
 const InnerCell = props => {
-  const {field, handleAnywhereClick, index, interactive, draggable, readonly} = props
+  const {
+    field,
+    handleAnywhereClick,
+    index,
+    interactive,
+    draggable,
+    readonly,
+    didDrop,
+    isOver,
+    connectDropTarget,
+    handleDragDropOnInput,
+    droppedItem = null
+  } = props
+
+  useEffect(() => {
+    if (didDrop && !previousDrop.current.didDrop && !isOver && previousDrop.current.isOver) {
+      // clone these objects before we send them up, we don't want them mutating them and causing unexpected behavior down here - JRA 12/05/2019
+      const source = typeof droppedItem === 'object' && typeof droppedItem.widget === 'object'
+        ? {...droppedItem.widget} : null
+      const target = typeof field === 'object' && typeof field.config === 'object'
+        ? {...field.config} : null
+      handleDragDropOnInput({source, target})
+    }
+  }, [didDrop, isOver, handleDragDropOnInput, field, droppedItem])
+
+  const previousDrop = useRef({didDrop: false, isOver: false})
+  useEffect(() => {
+    previousDrop.current = {
+      didDrop,
+      isOver
+    }
+  }, [didDrop, isOver])
+
   const {config} = field
   const [formValues] = useContext(FormValueContext)
 
@@ -21,7 +54,7 @@ const InnerCell = props => {
 
   const Type = mapInputType(config.type, interactive)
 
-  return (
+  return connectDropTarget(
     <div className='gfb-inner-cell' onClick={onGridElementClick}>
       <LabelContainer config={config} />
       <InputContainer config={config}>
@@ -31,7 +64,24 @@ const InnerCell = props => {
   )
 }
 
-export default InnerCell
+function collect (connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    droppedItem: monitor.getDropResult(),
+    didDrop: monitor.didDrop(),
+    isOver: monitor.isOver()
+  }
+}
+
+const boxTarget = {
+  drop (props, monitor) {
+    return {
+      widget: monitor.getItem()
+    }
+  }
+}
+
+export default DropTarget('FormBuilderDraggable', boxTarget, collect)(InnerCell)
 
 InnerCell.propTypes = {
   field: PropTypes.object,
@@ -39,5 +89,7 @@ InnerCell.propTypes = {
   index: PropTypes.number,
   interactive: PropTypes.bool,
   draggable: PropTypes.bool,
-  readonly: PropTypes.bool
+  readonly: PropTypes.bool,
+  didDrop: PropTypes.bool,
+  handleDragDropOnInput: PropTypes.func
 }
