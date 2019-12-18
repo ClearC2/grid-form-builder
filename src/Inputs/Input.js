ea59154aeaeb5791343d8086f2daa870d9d6fdc3
Jacob Allen <jacobrallen1@gmail.com>
2019-12-18 17:02:04 -0600

    feat(styles): adds basic css rules to start trying to think of a way to theme this

--- src/Inputs/Input.js
+++ src/Inputs/Input.js
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types'
 const Input = props => {
   const {name, value, onChange} = props
   return (
-    <input
-      name={name}
-      value={value}
-      onChange={onChange}
-    />
+    <div className='gfb-input-outer'>
+      <div className='gfb-input-inner'>
+        <div className='gfb-input__control'>
+          <div className='gfb-input__value-container'>
+            <input
+              className='gfb-input__single-value gfb-input__input'
+              name={name}
+              value={value}
+              onChange={onChange}
+            />
+          </div>
+          <div className='gfb-input-indicators' />
+        </div>
+      </div>
+    </div>
   )
 }
 
