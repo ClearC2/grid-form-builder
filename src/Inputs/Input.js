04a43c5cdfd4f4778b592e5c36ff9e29342da1ce
Jacob Allen <jacobrallen1@gmail.com>
2020-01-07 10:46:12 -0600

    feat(readonly): adds readonly classes to each defined input so far

--- src/Inputs/Input.js
+++ src/Inputs/Input.js
@@ -2,17 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Input = props => {
-  const {name, value, onChange} = props
+  const {name, value, onChange, readonly, disabled} = props
+  let className = 'gfb-input__single-value gfb-input__input'
+  if (readonly || disabled) className = className + ' gfb-disabled-input'
   return (
     <div className='gfb-input-outer'>
       <div className='gfb-input-inner'>
         <div className='gfb-input__control'>
           <div className='gfb-input__value-container'>
             <input
-              className='gfb-input__single-value gfb-input__input'
+              className={className}
               name={name}
               value={value}
               onChange={onChange}
+              disabled={readonly || disabled}
             />
           </div>
           <div className='gfb-input-indicators' />
@@ -27,5 +30,7 @@ export default Input
 Input.propTypes = {
   onChange: PropTypes.func,
   name: PropTypes.string,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array, PropTypes.object])
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.array, PropTypes.object]),
+  disabled: PropTypes.bool,
+  readonly: PropTypes.bool
 }
