ae319001988e4950e70861d513bd35887e3dcaf4
Jacob Allen <jacobrallen1@gmail.com>
2020-01-16 14:48:31 -0600

    feat(import): removed depricated importselect defintiion

--- src/FieldDefinitions/index.js
+++ src/FieldDefinitions/index.js
@@ -1,14 +1,12 @@
 import Conditionalinput from './Conditionalinput'
 import Header from './Header'
 import Icon from './Icon'
-import ImportSelect from './ImportSelect'
 import Metadata from './Metadata'
 
 let FormComponents = {
   Conditionalinput,
   Header,
   Icon,
-  ImportSelect,
   Metadata
 }
 
