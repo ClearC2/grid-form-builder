cbcfb2c86dcfe9af8b7ae01d52fe894d81780803
Jacob Allen <jacobrallen1@gmail.com>
2020-01-16 10:08:42 -0600

    feat(total): removes depricated total input type

--- src/FieldDefinitions/index.js
+++ src/FieldDefinitions/index.js
@@ -3,15 +3,13 @@ import Header from './Header'
 import Icon from './Icon'
 import ImportSelect from './ImportSelect'
 import Metadata from './Metadata'
-import Total from './Total'
 
 let FormComponents = {
   Conditionalinput,
   Header,
   Icon,
   ImportSelect,
-  Metadata,
-  Total
+  Metadata
 }
 
 export {FormComponents}
