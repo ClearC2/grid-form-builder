0919ddd76bb5ec6654296315fcc6c49663416c71
Jacob Allen <jacobrallen1@gmail.com>
2020-01-17 11:03:33 -0600

    feat(validation): adds validation warning to textarea

--- src/Inputs/Textarea.js
+++ src/Inputs/Textarea.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import PropTypes from 'prop-types'
+import ValidationErrorIcon from '../ValidationErrorIcon'
 
 const Textarea = props => {
   const {
@@ -12,17 +13,34 @@ const Textarea = props => {
     placeholder,
     tabIndex,
     autoComplete,
-    interactive = true
+    interactive = true,
+    requiredWarning
   } = props
+
+  const [isFocused, setIsFocused] = useState(false)
+
+  const handleOnFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleOnBlur = useCallback(() => {
+    setIsFocused(false)
+  }, [])
 
   let className = 'gfb-input__single-value gfb-input__input'
   if (readonly || disabled || !interactive) className = className + ' gfb-disabled-input'
   if (!interactive) className = className + ' gfb-non-interactive-input'
+  let controlClass = 'gfb-input__control'
+  let validationError
+  if (requiredWarning && (value + '').length === 0 && !isFocused) {
+    controlClass = controlClass + ' gfb-validation-error'
+    validationError = 'This field is required'
+  }
 
   return (
     <div className='gfb-input-outer'>
       <div className='gfb-input-inner'>
-        <div className='gfb-input__control'>
+        <div className={controlClass}>
           <div className='gfb-input__value-container'>
             <textarea
               className={className}
@@ -34,9 +52,13 @@ const Textarea = props => {
               placeholder={placeholder}
               tabIndex={tabIndex}
               autoComplete={autoComplete}
+              onFocus={handleOnFocus}
+              onBlur={handleOnBlur}
             />
           </div>
-          <div className='gfb-input__indicators' />
+          <div className='gfb-input__indicators'>
+            {validationError && <ValidationErrorIcon message={validationError} />}
+          </div>
         </div>
       </div>
     </div>
