f67c88f3e4460681843e3a60f17b255cf4d55b98
Will Darden <wldarden@gmail.com>
2020-02-04 16:21:14 -0600

    revert(condInput): to where it was

--- src/Inputs/ConditionalInput.js
+++ src/Inputs/ConditionalInput.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import ConditionalDialog from './ConditionalDialog'
 import {Map, List, fromJS} from 'immutable'
 const ConditionalInput = props => {
-  const {style = {}, name, value, onChange} = props
+  const {style = {}} = props
 
   const {value: valueStyle = {}, inputOuter = {}, inputInner = {}, inputControl = {}, valueContainer = {}, indicators = {}} = style// eslint-disable-line
 
@@ -14,25 +14,21 @@ const ConditionalInput = props => {
   }, [])
 
   useEffect(() => {
-    if (name) {
+    // const v = props.values[props.name]
+    if (props.name) {
       let defaults = Map({condition: 'contains', values: List()})
-      if (typeof value === 'string') {
-        if (value !== '') {
-          defaults = defaults.set('values', List([value]))
+      if (typeof props.value === 'string') {
+        if (props.value !== '') {
+          defaults = defaults.set('values', List([props.value]))
         } else {
           defaults = defaults.set('values', List())
         }
-      } else if (value instanceof List || Array.isArray(value)) {
-        defaults = defaults.set('values', fromJS(value))
+      } else if (props.value instanceof List || Array.isArray(props.value)) {
+        defaults = defaults.set('values', fromJS(props.value))
       }
-      let oldValue = !value ? Map() : value
-      oldValue = oldValue.toJS ? oldValue : fromJS(oldValue)
-      const newValue = fromJS(defaults)
-      if (!oldValue.equals(newValue)) {
-        onChange({target: {name: name, value: defaults}})
-      }
+      props.onChange({target: {name: props.name, value: defaults}})
     }
-  }, [name])
+  }, [props.name])
 
   return (
     <div className='gfb-input-outer' style={inputOuter}>
