882ccf6f655f6cc5bf882b190abd34ac957a3228
Jacob Allen <jacobrallen1@gmail.com>
2020-05-18 09:27:37 -0500

    feat(number): number area now accepts maxlength

--- src/Inputs/Number.js
+++ src/Inputs/Number.js
@@ -27,7 +27,8 @@ const Number = props => {
     interactive = true,
     requiredWarning,
     style = {},
-    required
+    required,
+    maxlength = 524288
   } = props
 
   const {
@@ -84,6 +85,10 @@ const Number = props => {
     controlClass = controlClass + ' gfb-validation-error'
     validationError = 'This Field is Required'
   }
+  let validationWarning
+  if (maxlength && (value + '').length && (value + '').length >= maxlength) {
+    validationWarning = `Maximum character limit of ${maxlength} reached.`
+  }
   let outerClass = 'gfb-input-outer'
   if (isFocused) {
     outerClass = outerClass + ' gfb-has-focus'
@@ -116,9 +121,14 @@ const Number = props => {
               onBlur={handleOnBlur}
               style={valueStyle}
               css={theme.value}
+              maxLength={maxlength}
             />
           </div>
           <div className='gfb-input__indicators' style={indicators} css={theme.indicators}>
+            {validationWarning && <ValidationErrorIcon message={validationWarning} color='#FFCC00' type='warning' />}
+            {validationWarning && validationError && (
+              <span className='gfb-input__indicator-separator css-1okebmr-indicatorSeparator' />
+            )}
             {validationError && <ValidationErrorIcon message={validationError} />}
           </div>
         </div>
@@ -148,5 +158,6 @@ Number.propTypes = {
   interactive: PropTypes.bool,
   requiredWarning: PropTypes.bool,
   style: PropTypes.object,
-  required: PropTypes.bool
+  required: PropTypes.bool,
+  maxlength: PropTypes.number
 }
