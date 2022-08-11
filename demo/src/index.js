912b17719fc71ec9cd3b3d8994bc71701e1fc89d
William <wjones@clearc2.com>
2022-08-11 10:53:37 -0500

    fix(deps): update dnd packages

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import store from '../../example/src/store'
 import {DndProvider} from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
+import {HTML5Backend} from 'react-dnd-html5-backend'
 import React from 'react'
 import Sandbox from './Sandbox'
 
