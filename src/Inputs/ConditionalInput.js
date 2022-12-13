65a414d3d67ef54a420a3694f084a2caf2b216ce
Lee Sanders <lksanders@c2crm.local>
2022-12-13 14:57:03 -0600

    fix(addFiscal): add fiscal year to cond year input

--- src/Inputs/ConditionalInput.js
+++ src/Inputs/ConditionalInput.js
@@ -38,7 +38,7 @@ const ConditionalInput = props => {
   const cond = values.getIn([name, 'condition'], '')
   const vals = values.getIn([name, 'values'], List())
   const hasValue = vals.size > 0 || cond.includes('blank') ||
-    cond === 'today' || cond === 'this month' || cond === 'year to date' || cond === 'fiscal year to date' ||
+    cond === 'today' || cond === 'this month' || cond === 'year to date' || cond === 'fiscal year to date' || cond === 'fiscal year' ||
     cond === 'this quarter' || cond === 'quarter to date' || cond === 'this week' ||
     (values.getIn([name, 'dynamicValues']) && values.getIn([name, 'dynamicValues']).size) ||
     (values.getIn([name, 'conditions'], List()).size > 0)
