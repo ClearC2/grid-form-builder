045a5bc6cc6c5ede25d5fed7fb4eda81286ac4cd
William Jones <wjones@clearc2.com>
2025-06-05 10:21:19 -0500

    fix(hide-tooltip): hide tooltip when typing or scrolling (#245)

---
+++ src/useAutoHideTooltip.js
@@ -0,0 +1,43 @@
+import {useEffect} from 'react'
+import ReactTooltip from 'react-tooltip'
+
+// Keep track of how many active consumers
+// so can have many forms but only add event listeners once
+// removes them if we get to a zero count
+let attachmentCount = 0
+
+function hideToolTip (e) {
+  const target = e.target
+
+  if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || e.type === 'scroll') {
+    ReactTooltip.hide()
+  }
+}
+
+function registerListeners () {
+  if (attachmentCount === 0) {
+    window.addEventListener('scroll', hideToolTip, {passive: true, capture: true})
+    window.addEventListener('input', hideToolTip, true)
+    window.addEventListener('keydown', hideToolTip, true)
+  }
+  attachmentCount += 1
+}
+
+function unregisterListeners () {
+  attachmentCount -= 1
+
+  if (attachmentCount <= 0) {
+    window.removeEventListener('scroll', hideToolTip, true)
+    window.removeEventListener('input', hideToolTip, true)
+    window.removeEventListener('keydown', hideToolTip, true)
+  }
+}
+
+export const useAutoHideTooltip = () => {
+  useEffect(() => {
+    registerListeners()
+    return () => {
+      unregisterListeners()
+    }
+  }, [])
+}
