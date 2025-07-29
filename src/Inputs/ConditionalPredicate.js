c42e0480448a1c4b5a8200e4339328e75bb1fbec
Hargis32 <apratt@clearc2.com>
2025-07-29 12:47:29 -0500

    feat(reports): add new is one of option (#248)
    Co-authored-by: Austin Pratt <apratt@c2crm.local>

--- src/Inputs/ConditionalPredicate.js
+++ src/Inputs/ConditionalPredicate.js
@@ -281,7 +281,8 @@ const ConditionalPredicate = props => {
             category: 'NONE',
             options: [
               {label: '{Logged on User}', value: '{Logged on User}'},
-              {label: '{Reports to Logged on User}', value: '{Reports to Logged on User}'}
+              {label: '{Reports to Logged on User}', value: '{Reports to Logged on User}'},
+              {label: '{Full Reports to Logged on User}', value: '{Full Reports to Logged on User'}
             ]
           }
         }
